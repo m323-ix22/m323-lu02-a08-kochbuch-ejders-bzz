5f46004cf9cc741a3dee32f45ed75f685b7bc0cd
Hustini <ejdersalih02@gmail.com>
2024-10-30 14:37:18 +0100

    Done

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-# Dein Code kommt hier hin
+"""Code Here"""
 
-if __name__ == '__main__':
+import json
+
+
+def adjust_recipe(recipe, number):
+    data = load_recipe(recipe)
+    data["servings"] = number
+    for i in data["ingredients"]:
+        data["ingredients"][i] = int(data["ingredients"][i] / number)
+    return data
+
+
+def load_recipe(recipe):
+    if isinstance(recipe, dict):
+        return recipe
+    return json.loads(recipe)
+
+if __name__ == "__main__":
     # Beispiel für die Datenstruktur eines Rezepts
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    recipe_json = ('{"title": "Spaghetti Bolognese", "ingredients": '
+                   '{"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}')
+    print(adjust_recipe(recipe_json, 2))
